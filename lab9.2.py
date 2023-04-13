3c4cf3b36c76b3b341adcc6e7389639d92421a29
Assel Amanzholova <amanzholova-2003@inbox.ru>
2023-04-13 12:00:18 +0600

    lab9.2.py added

---
+++ lab9.2.py
@@ -0,0 +1,13 @@
+def get_data():
+    my_list = [1, 2, 3]
+    my_tuple = (4, 5, 6)
+    my_dict = {"name": "Assel", "age": 19}
+    return my_list, my_tuple, my_dict
+
+result_list, result_tuple, result_dict = get_data()
+
+print(result_list)
+print(result_tuple)
+print(result_dict)
+
+    
